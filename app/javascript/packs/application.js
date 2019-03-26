861d30e8e0924e33cbdc64ad0963f8af8ab1c18e
rawdan <bogdancrudu@yahoo.com>
2019-03-26 17:08:37 +0200

    add tweets basic MVC functionality

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,3 +23,11 @@ import "controllers"
 
 require("trix")
 require("@rails/actiontext")
+
+// src/application.js
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_20_143034) do
+ActiveRecord::Schema.define(version: 2019_03_26_145941) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -101,6 +101,14 @@ ActiveRecord::Schema.define(version: 2019_03_20_143034) do
     t.index ["user_id"], name: "index_services_on_user_id"
   end
 
+  create_table "tweets", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -119,4 +127,5 @@ ActiveRecord::Schema.define(version: 2019_03_20_143034) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "services", "users"
+  add_foreign_key "tweets", "users"
 end
