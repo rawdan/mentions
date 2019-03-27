13f15d2c85321dd4f00519f941d2d94a7b721253
rawdan <bogdancrudu@yahoo.com>
2019-03-27 16:01:23 +0200

    finish adding dynamic nested forms

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -10,9 +10,28 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "output" ]
+  static targets = [ "links", "template" ]
 
   connect() {
-    this.outputTarget.textContent = 'Hello, Stimulus!'
+
+  }
+
+  add_association(event) {
+    event.preventDefault()
+
+    var content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime())
+    this.linksTarget.insertAdjacentHTML('beforebegin', content)
+  }
+
+  remove_association(event) {
+    event.preventDefault()
+
+    let wrapper = event.target.closest(".nested-fields")
+    if (wrapper.dataset.newRecord == "true") {
+      wrapper.remove()
+    } else {
+      wrapper.querySelector("input[name*='_destroy']").value = 1
+      wrapper.style.display = 'none'
+    }
   }
 }
